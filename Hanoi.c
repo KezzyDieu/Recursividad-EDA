ccdbb8f74e1bc630d022a447e80e0a5a6ea70b7e
Evsoto71 . <evansm.ti23@utsjr.edu.mx>
2024-09-17 08:40:09 -0600

    first commit

---
+++ Hanoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void hanoi(int n, char origen, char destino, char auxiliar) {
+    if (n == 1) {
+        printf("Mover disco 1 de %c a %c\n", origen, destino);
+        return;
+    }
+    hanoi(n - 1, origen, auxiliar, destino);
+    printf("Mover disco %d de %c a %c\n", n, origen, destino);
+    hanoi(n - 1, auxiliar, destino, origen);
+}
+
+int main() {
+    int n;
+    printf("Introduce el numero de discos: ");
+    scanf("%d", &n);
+    hanoi(n, 'A', 'C', 'B');  
+    return 0;
+}
